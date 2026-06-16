09a5f7f9da4a7b61861bdb3f7fa05792a5a4c1c1
Conrad008 <kipngeno7180@gmail.com>
2026-06-16 19:28:30 +0300

    feat: add  mobile menu toggle logic

---
+++ js/index.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const mobileMenuBtn = document.getElementById('mobileMenuBtn');
+    const mobileMenuDrawer = document.getElementById('mobileMenuDrawer');
+
+    if(mobileMenuBtn && mobileMenuDrawer){
+        mobileMenuBtn.addEventListener('click', () => {
+
+            mobileMenuDrawer.classList.toggle('hidden');
+
+            console.log('Menu toggled! Current classes:', mobileMenuDrawer.className);
+        });
+    } else{
+        console.warn('menu links not found');
+    }
+});
