840db41bb0479dfff52abb7594ec50f4e9242e95
Conrad008 <kipngeno7180@gmail.com>
2026-06-17 13:39:58 +0300

    feat:Add validation for login form email field

--- js/login.js
+++ js/login.js
@@ -3,4 +3,29 @@ document.addEventListener('DOMContentLoaded', () => {
     const emailInput = document.getElementById('email');
     const passwordInput = document.getElementById('password');
 
+    const showInputError = (inputEl) => {
+    inputEl.classList.add('border-red-500', 'focus:ring-red-500', 'focus:border-red-500');
+    inputEl.classList.remove('border-white/50', 'focus:ring-thirdPalette', 'focus:border-thirdPalette');
+    };
+
+    const clearInputError = (inputEl) => {
+    inputEl.classList.remove('border-red-500', 'focus:ring-red-500', 'focus:border-red-500');
+    inputEl.classList.add('border-white/50', 'focus:ring-thirdPalette', 'focus:border-thirdPalette');
+    };
+
+    loginForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        let isValid = true;
+
+        const emailValue = emailInput.trim();
+        if (emailValue === ""){
+            showInputError(emailInput)
+            isValid = false
+        }
+        else{
+            clearInputError(emailInput)
+        }
+    })
+
 })
