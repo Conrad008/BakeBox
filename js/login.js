87fa2f3afeeb99c16f74705b3d233ef0cc5f097c
Conrad008 <kipngeno7180@gmail.com>
2026-06-17 12:40:09 +0300

    feat: Initialize DOM element selectors for login form

---
+++ js/login.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('loginForm');
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+
+})
