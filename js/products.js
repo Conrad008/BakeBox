ef57f61e76b6b234d81f73378e045b08301f8c1d
Conrad008 <kipngeno7180@gmail.com>
2026-06-18 19:30:33 +0300

    feat: add toggle hamburger button functionality

--- js/products.js
+++ js/products.js
@@ -5,6 +5,22 @@ document.addEventListener('DOMContentLoaded', () => {
     fetchProducts(productsGrid);
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+    const mobileMenuBtn = document.getElementById('mobileMenuBtn');
+    const mobileMenuDrawer = document.getElementById('mobileMenuDrawer');
+
+    if(mobileMenuBtn && mobileMenuDrawer){
+        mobileMenuBtn.addEventListener('click', () => {
+
+            mobileMenuDrawer.classList.toggle('hidden');
+
+            console.log('Menu toggled! Current classes:', mobileMenuDrawer.className);
+        });
+    } else{
+        console.warn('menu links not found');
+    }
+});
+
 async function fetchProducts(gridElement){
     if (!gridElement) return;
 
