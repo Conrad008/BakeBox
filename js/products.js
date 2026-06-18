a42e413a2cf55c3c69e8d525d741ad8ac0a890cb
Conrad008 <kipngeno7180@gmail.com>
2026-06-18 18:02:49 +0300

    Feat:add functions torender individual products in styled cards

--- js/products.js
+++ js/products.js
@@ -19,3 +19,41 @@ async function fetchProducts(gridElement){
         gridElement.innerHTML = `<p class="text-red-500 text-center col-span-full">Unable to load pastries right now. Please try refreshing.</p>`;
     }
 }
+
+function renderProducts (products, gridElement){
+    gridElement.innerHTML = '';
+
+    products.forEach(product => {
+        const card = document.createElement('div');
+
+        card.className = "bg-white rounded-xl shadow-sm border border-stone-100 overflow-hidden flex flex-col justify-between transition transform hover:-translate-y-1 hover:shadow-md duration-300";
+
+        card.innerHTML = `
+        <div>
+
+        <div class="h-48 w-full overflow-hidden bg-stone-100 relative">
+                    <img src="${product.image}" alt="${product.name}" class="w-full h-full object-cover" onerror="this.src='https://placehold.co/400x300?text=Fresh+Bake'">
+                    <span class="absolute top-2 right-2 bg-amber-500/90 text-white text-xs font-semibold px-2 py-1 rounded">
+                        ${product.rating}/10
+                    </span>
+                </div>
+
+                <div class="p-4">
+                    <span class="text-xs font-bold uppercase tracking-wider text-amber-600">${product.category}</span>
+                    <h3 class="text-lg font-bold text-stone-800 mt-1">${product.name}</h3>
+                </div>
+
+        </div>
+        <div class = "p-4 pt-0 flex items-center justify-between mt-4">
+            <span class="text-xl font-black text-stone-900">KSH${product.price.toFixed(2)}
+            </span>
+            <button data-id="${product.id}" class="add-to-cart-btn bg-amber-600 hover:bg-amber-700 text-white text-sm font-medium px-4 py-2 rounded-lg transition dynamic-colors">
+             Add to Cart
+            </button>
+        </div>
+        `;
+
+        gridElement.appendChild(card);
+    });
+
+}
