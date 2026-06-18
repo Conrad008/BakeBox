65f2e584d6d0ceae2e3dc55d2117fb1495c46e7a
Conrad008 <kipngeno7180@gmail.com>
2026-06-18 11:42:11 +0300

    feat: declare variables and initialize DOM selectors

---
+++ js/register.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const registerForm = document.getElementById('registerForm');
+    const nameInput = document.getElementById('fullName');
+    const emailInput = document.getElementById('email');
+    const phoneInput = document.getElementById('phone');
+    const passwordInput = document.getElementById('password');
+    
+    const togglePasswordBtn = document.getElementById('togglePassword');
+    const eyeIcon = document.getElementById('eyeIcon');
+    const successMessage = document.getElementById('formSuccess');
+
+});
