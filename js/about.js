ecccf77f79b61a11897fbee4d292b20fc8826ec9
Conrad008 <kipngeno7180@gmail.com>
2026-06-20 17:31:44 +0300

    feat: add update cart badge and hamburger menu functionality

---
+++ js/about.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', () => {
+            const cart = JSON.parse(localStorage.getItem('BakeBox_Cart')) || [];
+            const badge = document.getElementById('cartCountBadge');
+            if (badge) {
+            const totalItems = cart.reduce((tally, item) => tally + item.quantity, 0);
+            if (totalItems > 0) {
+            badge.textContent = totalItems;
+            badge.classList.remove('hidden');
+        }
+    }
+
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const mobileMenuBtn = document.getElementById('mobileMenuBtn');
+    const mobileMenuDrawer = document.getElementById('mobileMenuDrawer');
+
+    if(mobileMenuBtn && mobileMenuDrawer){
+        mobileMenuBtn.addEventListener('click', () => {
+
+            mobileMenuDrawer.classList.toggle('hidden');
+
+            console.log('Menu toggled! Current classes:', mobileMenuDrawer.className);
+        });
+    } else{
+        console.warn('menu links not found');
+    }
+});
